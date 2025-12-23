e83b5b35b52370d81a68c35bb84a3b0f202d183e
MescoCzubinski <mieszkoczubinski@gmail.com>
2025-12-24 00:21:00 +0100

    desktop v1

--- configuration.nix
+++ configuration.nix
@@ -6,33 +6,16 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [ 
       ./hardware-configuration.nix
     ];
 
-  # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "nixos"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Set your time zone.
   time.timeZone = "Europe/Warsaw";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
   i18n.extraLocaleSettings = {
     LC_ADDRESS = "pl_PL.UTF-8";
     LC_IDENTIFICATION = "pl_PL.UTF-8";
@@ -45,27 +28,18 @@
     LC_TIME = "pl_PL.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
+  programs.hyprland.enable = true;
   services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.wayland.enable = true;
 
-  # Configure keymap in X11
+  console.keyMap = "pl2";
   services.xserver.xkb = {
     layout = "pl";
     variant = "";
   };
 
-  # Configure console keymap
-  console.keyMap = "pl2";
-
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -73,69 +47,101 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  services.libinput.enable = true;
   users.users.mieszko = {
     isNormalUser = true;
     description = "mieszko";
     extraGroups = [ "networkmanager" "wheel" ];
-    packages = with pkgs; [
-      kdePackages.kate
-    #  thunderbird
-    ];
   };
 
-  # Install firefox.
-  programs.firefox.enable = true;
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+  };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  programs.git = {
+    enable = true;
+    config = {
+      user.name = "MescoCzubinski";
+      user.email = "mieszkoczubinski@gmail.com";      
+      init.defaultBranch = "main";
+    };
+  };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-   wget
-	brave
-git
-vscode
+  fonts.packages = with pkgs; [
+    fira-code
+    fira-code-symbols
+    nerd-fonts.fira-code
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  fonts.fontconfig = {
+    defaultFonts = {
+      monospace = [ "FiraCode Nerd Font" ];
+      sansSerif = [ "Fira Sans" ];
+      serif     = [ "Fira Serif" ];
+    };
+  };
 
-  # List services that you want to enable:
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    # code editors
+    code-cursor
+    vscode
+    
+    # dev tools
+    docker
+    yaak
+    git
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+    # languages
+    nodejs
+    python3
+    go
 
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
+    # social
+    discord
+    signal-desktop
+    spotify
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.11"; # Did you read the comment?
+    # utilities
+    onlyoffice-desktopeditors
+	  brave
 
+    # terminal
+    kitty
+
+    # desktop environment
+    waybar # task bar
+    wlogout # logout screen
+    superfile # file manager
+    hypridle # power management
+    hyprpaper # wallpaper manager
+    hyprlock # screen locker
+    rofi # application launcher
+    mako # notification daemon
+
+    # screenshots
+    grim
+    slurp
+    wl-clipboard
+    
+    neofetch # fancy terminal info
+
+    networkmanagerapplet # network manager
+    blueman # bluetooth manager
+    bibata-cursors # cursor theme
+  ];
+
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = true;
+
+  virtualisation.docker.enable = true;
+
+  system.stateVersion = "25.11";
 }
